f5688fd92963135087a1f2c618bbfe2be031df3c
Mnx Pro Gaming <mnxprogaming@gmail.com>
2026-08-27 13:56:51 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running 24/7!"
+
+def run():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
