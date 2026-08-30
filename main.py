b7c0c3b7d63fce49ad959cd473c3d607614819b3
Mnx Pro Gaming <mnxprogaming@gmail.com>
2026-08-30 14:53:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -940,7 +940,7 @@ def image_ai_handler(message):
 
         # Gemini ko image + prompt bhejo
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=[
                 {
                     "inline_data": {
